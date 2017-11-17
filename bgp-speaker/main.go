b9ae9e01278befca1d6b33f19995bdaef6adcdd6
David Anderson <dave@natulte.net>
2017-11-17 07:53:05 -0800

    Checkpoint BGP speaker code.
    This code can create BGP sessions with gobgp. However, I'm increasingly
    unhappy with gobgp, to the point where I'm going to write my own tiny BGP
    library that does just what I need.

--- bgp-speaker/main.go
+++ bgp-speaker/main.go
@@ -16,88 +16,151 @@ package main
 
 import (
 	"flag"
-	"sync"
+	"fmt"
+	"reflect"
 
-	"go.universe.tf/metallb/internal"
+	"go.universe.tf/metallb/internal/config"
+	"go.universe.tf/metallb/internal/k8s"
 
 	"github.com/golang/glog"
+	"github.com/kr/pretty"
+	bgpconfig "github.com/osrg/gobgp/config"
+	"github.com/osrg/gobgp/server"
 
 	"k8s.io/api/core/v1"
-	"k8s.io/client-go/tools/record"
 )
 
-type speaker struct {
-	nodeName string
-	events   record.EventRecorder
-
-	sync.Mutex
-	svcs map[string]bool
+type service interface {
+	Infof(svc *v1.Service, desc, msg string, args ...interface{})
+	Errorf(svc *v1.Service, desc, msg string, args ...interface{})
 }
 
-func (s *speaker) UpdateBalancer(name string, svc *v1.Service, eps *v1.Endpoints) error {
-	if svc.Spec.Type != "LoadBalancer" {
-		return s.deleteBalancer(name, "not a LoadBalancer")
-	}
-	if _, ok := svc.Annotations[internal.AnnotationAssignedIP]; !ok {
-		return s.deleteBalancer(name, "no IP assigned yet")
+type controller struct {
+	client service
+	config *config.Config
+	peers  []peer
+}
+
+type peer struct {
+	cfg *config.Peer
+	bgp *server.BgpServer
+}
+
+func (c *controller) SetBalancer(name string, svc *v1.Service, eps *v1.Endpoints) error {
+	if svc == nil {
+		return c.deleteBalancer(name)
 	}
 
-	pods := map[string]bool{}
-	for _, subset := range eps.Subsets {
-		for _, a := range subset.Addresses {
-			if a.NodeName != nil && *a.NodeName == s.nodeName {
-				pods[a.IP] = true
+	if svc.Spec.Type != "LoadBalancer" {
+		return nil
+	}
+
+	if c.config == nil {
+		// Config hasn't been read, nothing we can do just yet.
+		glog.Infof("%q skipped, no config loaded", name)
+		return nil
+	}
+
+	glog.Infof("TODO: do stuff with balancer")
+
+	return nil
+}
+
+func (c *controller) deleteBalancer(name string) error {
+	glog.Infof("TODO: delete balancer")
+	return nil
+}
+
+func (c *controller) SetConfig(cfg *config.Config) error {
+	pretty.Printf("Newconfig! %# v\n", cfg)
+
+	newPeers := make([]peer, len(cfg.Peers))
+	for i, p := range cfg.Peers {
+		if i <= len(c.peers)-1 {
+			if reflect.DeepEqual(p, c.peers[i].cfg) {
+				newPeers[i] = c.peers[i]
+				c.peers[i].bgp = nil
+				continue
 			}
 		}
+
+		newPeers[i].cfg = &p
+		glog.Infof("New BGP peer %#v", p)
+		s, err := mkBGP(p)
+		if err != nil {
+			return fmt.Errorf("creating BGP session for %q: %s", p.Addr, err)
+		}
+		newPeers[i].bgp = s
 	}
-	for _, subset := range eps.Subsets {
-		for _, a := range subset.NotReadyAddresses {
-			delete(pods, a.IP)
+
+	c.config = cfg
+	oldPeers := c.peers
+	c.peers = newPeers
+	for _, p := range oldPeers {
+		if p.bgp == nil {
+			continue
+		}
+		if err := p.bgp.Stop(); err != nil {
+			return fmt.Errorf("shutting down BGP session to %q: %s", p.cfg.Addr, err)
 		}
 	}
-
-	if len(pods) == 0 {
-		return s.deleteBalancer(name, "no serving endpoints on this node")
-	}
-
-	s.Lock()
-	defer s.Unlock()
-	glog.Infof("UPDATE service %q, %v -> %s (weight %d)", name, svc.Annotations[internal.AnnotationAssignedIP], s.nodeName, len(pods))
-	s.svcs[name] = true
+	glog.Infof("New config loaded")
 	return nil
 }
 
-func (s *speaker) DeleteBalancer(name string) error {
-	return s.deleteBalancer(name, "service deleted")
-}
+func mkBGP(cfg config.Peer) (*server.BgpServer, error) {
+	s := server.NewBgpServer()
+	go s.Serve()
 
-func (s *speaker) deleteBalancer(name, reason string) error {
-	s.Lock()
-	defer s.Unlock()
-
-	if s.svcs[name] {
-		glog.Infof("DELETE service %q (%s)", name, reason)
+	bgpCfg := &bgpconfig.Global{
+		Config: bgpconfig.GlobalConfig{
+			As:       cfg.MyASN,
+			RouterId: "127.0.0.1", // TODO
+			Port:     -1,          // Don't listen anywhere, we only connect out.
+		},
 	}
-	delete(s.svcs, name)
-	return nil
+	if err := s.Start(bgpCfg); err != nil {
+		return nil, fmt.Errorf("starting BGP server: %s", err)
+	}
+
+	n := &bgpconfig.Neighbor{
+		Config: bgpconfig.NeighborConfig{
+			NeighborAddress: cfg.Addr.String(),
+			PeerAs:          cfg.ASN,
+		},
+		Timers: bgpconfig.Timers{
+			Config: bgpconfig.TimersConfig{
+				ConnectRetry:           1.0,
+				IdleHoldTimeAfterReset: 1.0,
+			},
+		},
+	}
+	if err := s.AddNeighbor(n); err != nil {
+		return nil, fmt.Errorf("adding neighbor: %s", err)
+	}
+
+	if err := s.EnableNeighbor(cfg.Addr.String()); err != nil {
+		return nil, fmt.Errorf("starting neighbor: %s", err)
+	}
+
+	return s, nil
 }
+
+func (c *controller) MarkSynced() {}
 
 func main() {
 	kubeconfig := flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 	master := flag.String("master", "", "master url")
-	nodeName := flag.String("node-name", "", "name of the local node")
 	flag.Parse()
 
-	client, events, err := internal.Client(*master, *kubeconfig, "metallb-bgp-speaker")
+	c := &controller{}
+
+	client, err := k8s.NewClient("metallb-bgp-speaker", *master, *kubeconfig, c, false)
 	if err != nil {
 		glog.Fatalf("Error getting k8s client: %s", err)
 	}
 
-	s := &speaker{
-		nodeName: *nodeName,
-		events:   events,
-		svcs:     map[string]bool{},
-	}
+	c.client = client
 
-	glog.Fatal(internal.WatchServices(client, s))
+	glog.Fatal(client.Run())
 }
