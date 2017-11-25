9ababada2403d79588edadacd61253cc8711b759
David Anderson <dave@natulte.net>
2017-11-24 21:22:06 -0800

    Oops, add back the allocator code, I didn't mean to delete it.

---
+++ internal/allocator/allocator.go
@@ -0,0 +1,189 @@
+package allocator
+
+import (
+	"errors"
+	"fmt"
+	"net"
+
+	"go.universe.tf/metallb/internal/config"
+)
+
+// An Allocator tracks IP address pools and allocates addresses from them.
+type Allocator struct {
+	pools map[string]*config.Pool
+
+	svcToIP       map[string]net.IP
+	svcToPool     map[string]string
+	ipToSvc       map[string]string
+	poolAllocated map[string]int64
+}
+
+// New returns an Allocator managing no pools.
+func New() *Allocator {
+	return &Allocator{
+		pools: map[string]*config.Pool{},
+
+		svcToIP:       map[string]net.IP{},
+		svcToPool:     map[string]string{},
+		ipToSvc:       map[string]string{},
+		poolAllocated: map[string]int64{},
+	}
+}
+
+// SetPools updates the set of address pools that the allocator owns.
+func (a *Allocator) SetPools(pools map[string]*config.Pool) error {
+	for svc, ip := range a.svcToIP {
+		if poolFor(pools, svc, ip) == "" {
+			return fmt.Errorf("new config not compatible with assigned IPs: service %q cannot %q under new config", svc, ip)
+		}
+	}
+
+	a.pools = pools
+
+	// Need to readjust the existing pool mappings and counts
+	for svc, ip := range a.svcToIP {
+		pool := poolFor(a.pools, svc, ip)
+		if a.svcToPool[svc] != pool {
+			a.poolAllocated[a.svcToPool[svc]]--
+			a.svcToPool[svc] = pool
+			a.poolAllocated[a.svcToPool[svc]]++
+		}
+	}
+	return nil
+}
+
+// poolFor returns the pool that owns the requested IP, or "" if none.
+func poolFor(pools map[string]*config.Pool, service string, ip net.IP) string {
+	for pname, p := range pools {
+		if ipConfusesBuggyFirmwares(ip) && p.AvoidBuggyIPs {
+			continue
+		}
+		for _, cidr := range p.CIDR {
+			if cidr.Contains(ip) {
+				return pname
+			}
+		}
+	}
+	return ""
+}
+
+// assign records an assignment. It is the caller's responsibility to
+// verify that the assignment is permissible.
+func (a *Allocator) assign(service, pool string, ip net.IP) {
+	if !a.svcToIP[service].Equal(ip) {
+		a.poolAllocated[pool]++
+	}
+	a.svcToIP[service] = ip
+	a.svcToPool[service] = pool
+	a.ipToSvc[ip.String()] = service
+}
+
+// Assign marks service as the owner of ip, if that address is available.
+func (a *Allocator) Assign(service string, ip net.IP) error {
+	if other, ok := a.ipToSvc[ip.String()]; ok {
+		if other != service {
+			return fmt.Errorf("cannot assign %q to %q, already owned by %q", ip, service, other)
+		}
+		// IP already allocated correctly, nothing to do.
+		return nil
+	}
+	pool := poolFor(a.pools, service, ip)
+	if pool == "" {
+		return fmt.Errorf("cannot assign %q to %q, no pool owns that IP", ip, service)
+	}
+
+	// If the service already has another assignment, clear it. This
+	// is idempotent, so won't do harm if there's no allocation.
+	a.Unassign(service)
+	a.assign(service, pool, ip)
+	return nil
+}
+
+// allocateFromPool tries to allocate an IP from pool. Returns nil if no IPs are available.
+func (a *Allocator) allocateFromPool(service, pname string) net.IP {
+	pool := a.pools[pname]
+	for _, cidr := range pool.CIDR {
+		for ip := cidr.IP; cidr.Contains(ip); ip = nextIP(ip) {
+			if pool.AvoidBuggyIPs && ipConfusesBuggyFirmwares(ip) {
+				continue
+			}
+			if a.ipToSvc[ip.String()] == "" {
+				a.assign(service, pname, ip)
+				return ip
+			}
+		}
+	}
+
+	// No IPs available.
+	return nil
+}
+
+// AllocateFromPool assigns an availabe IP from pool to service.
+func (a *Allocator) AllocateFromPool(service, pool string) (net.IP, error) {
+	if a.pools[pool] == nil {
+		return nil, fmt.Errorf("pool %q does not exist", pool)
+	}
+	ip := a.allocateFromPool(service, pool)
+	if ip == nil {
+		return nil, fmt.Errorf("no addresses available in pool %q", pool)
+	}
+	return ip, nil
+}
+
+// Allocate assigns any available IP to service.
+func (a *Allocator) Allocate(service string) (net.IP, error) {
+	for pname := range a.pools {
+		if ip := a.allocateFromPool(service, pname); ip != nil {
+			return ip, nil
+		}
+	}
+	return nil, errors.New("no addresses available in any pool")
+}
+
+// Unassign frees the IP associated with service, if any.
+func (a *Allocator) Unassign(service string) bool {
+	ip := a.svcToIP[service]
+	if ip == nil {
+		// Service didn't have an assignment, nothing to do.
+		return false
+	}
+	a.poolAllocated[a.svcToPool[service]]--
+	delete(a.svcToIP, service)
+	delete(a.svcToPool, service)
+	delete(a.ipToSvc, ip.String())
+	return true
+}
+
+func (a *Allocator) GetIP(service string) net.IP {
+	return a.svcToIP[service]
+}
+
+func (a *Allocator) GetPool(service string) string {
+	return a.svcToPool[service]
+}
+
+// nextIP returns the next IP in sequence after prev.
+func nextIP(prev net.IP) net.IP {
+	var ip net.IP
+	ip = append(ip, prev...)
+	if ip.To4() != nil {
+		ip = ip.To4()
+	}
+	for o := 0; o < len(ip); o++ {
+		if ip[len(ip)-o-1] != 255 {
+			ip[len(ip)-o-1]++
+			return ip
+		}
+		ip[len(ip)-o-1] = 0
+	}
+	return ip
+}
+
+// ipConfusesBuggyFirmwares returns true if ip is an IPv4 address ending in 0 or 255.
+//
+// Such addresses can confuse smurf protection on crappy CPE
+// firmwares, leaving to packet drops.
+func ipConfusesBuggyFirmwares(ip net.IP) bool {
+	ip = ip.To4()
+	return ip[len(ip)-1] == 0 || ip[len(ip)-1] == 255
+}
