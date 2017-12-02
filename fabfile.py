71cc8f4d31034553a1b4ef4e9259e5985a39f111
David Anderson <dave@natulte.net>
2017-12-01 19:51:06 -0800

    A few tweaks to the fabfile, to make it work all nice. #11
    Notably, `fab push_manifests` and `fab push` no longer depends on
    the minikube binary, so you can run the hacking internal loop against
    any cluster you want, not just minikube. `fab start/stop` still build
    and destroy minikube clusters for quick development.

--- fabfile.py
+++ fabfile.py
@@ -14,18 +14,6 @@ def _silent(*args, **kwargs):
     with hide('warnings', 'running'):
         return local(*args, capture=True, **kwargs)
 
-def _minikube_running():
-    return _silent_nofail("minikube ip").succeeded
-
-def _registry_nodeport():
-    return _silent("minikube service --url -n kube-system registry")[7:]
-
-def _registry_clusterip():
-    return "%s:80" % _silent("kubectl get svc -n kube-system registry -o go-template='{{.spec.clusterIP}}'")
-
-def _kube_obj_exists(n):
-    return _silent_nofail("kubectl get %s").succeeded
-
 @contextlib.contextmanager
 def _tempdir():
     name = tempfile.mkdtemp()
@@ -34,46 +22,65 @@ def _tempdir():
     finally:
         shutil.rmtree(name)
 
-@contextlib.contextmanager
-def _proxy_to_registry():
-    p = subprocess.Popen("socat TCP-LISTEN:5000,fork,reuseaddr,retry=5 TCP:%s" % _registry_nodeport(), shell=True)
-    yield
-    p.kill()
+## Minikube bringup/teardown
+
+def _minikube_running():
+    return _silent_nofail("minikube ip").succeeded
 
 def start():
+    """Start minikube and configure it for MetalLB"""
     if not _minikube_running():
         local("minikube start")
-
     local("minikube addons enable registry")
     print("waiting for registry to start")
     while _silent_nofail("kubectl get svc -n kube-system registry").failed:
         time.sleep(1)
     regSvcType = _silent("kubectl get svc -n kube-system registry -o go-template=\"{{.spec.type}}\"")
-    if regSvcType != "NodePort":
-        local('kubectl patch svc -n kube-system registry -p \'{"spec":{"type":"NodePort"}}\'')
 
-    if not _kube_obj_exists("ns metallb-system"):
+    if _silent_nofail("kubectl get ns metallb-system").failed:
         push_manifests()
 
-def delete():
+def stop():
+    """Delete running minikube cluster"""
     if _minikube_running():
         local("minikube delete")
 
+## Platform-agnostic stuff - just uses kubectl and assumes the kube-system registry is running
+
+def _registry_clusterip():
+    return "%s:80" % _silent("kubectl get svc -n kube-system registry -o go-template='{{.spec.clusterIP}}'")
+
+def _kube_obj_exists(n):
+    return _silent_nofail("kubectl get %s").succeeded
+
+@contextlib.contextmanager
+def _proxy_to_registry():
+    registry_pod = _silent("kubectl get pod -n kube-system -l kubernetes.io/minikube-addons=registry -o go-template=\"{{(index .items 0).metadata.name}}\"")
+    p = subprocess.Popen("kubectl port-forward -n kube-system %s 5000:5000" % registry_pod, shell=True)
+    yield
+    p.kill()
+
 def push_manifests():
+    """Push the metallb binary manifests"""
     local("kubectl apply -f manifests/metallb.yaml")
 
 def _build(ts, name):
     with _tempdir() as tmp:
+        local("go install ./%s" % name)
         local("go build -o %s/%s ./%s" % (tmp, name, name))
         local("cp ./dockerfiles/dev/%s %s/Dockerfile" % (name, tmp))
+        local("sudo docker build -t localhost:5000/%s:latest %s" % (name, tmp))
+        local("sudo docker tag localhost:5000/%s:latest localhost:5000/%s:%s" % (name, name, ts))
         with _proxy_to_registry():
-            local("sudo docker build -t localhost:5000/%s:latest %s" % (name, tmp))
-            local("sudo docker tag localhost:5000/%s:latest localhost:5000/%s:%s" % (name, name, ts))
             local("sudo docker push localhost:5000/%s:%s" % (name, ts))
-            local("sudo docker rmi localhost:5000/%s:%s" % (name, ts))
+        local("sudo docker rmi localhost:5000/%s:%s" % (name, ts))
 
 
 def push():
+    """Build and repush metallb binaries"""
+    if _silent_nofail("kubectl get ns metallb-system").failed:
+        push_manifests()
+
     ts = "%f" % time.time()
     _build(ts, "controller")
     _build(ts, "bgp-speaker")
