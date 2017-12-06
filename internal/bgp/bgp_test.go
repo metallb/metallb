6b291beb5ea38cd753636fdfa732a729c7202e76
David Anderson <dave@natulte.net>
2017-12-05 22:18:00 -0800

    Add a basic interoperability test with GoBGP.
    The test harness running on Minikube is much more exhaustive,
    but the GoBGP test verifies basic functionality in <5s, and Travis
    can run it.

---
+++ internal/bgp/bgp_test.go
@@ -0,0 +1,134 @@
+package bgp
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"sort"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/osrg/gobgp/config"
+	gobgp "github.com/osrg/gobgp/server"
+	"github.com/osrg/gobgp/table"
+)
+
+func ipnet(s string) *net.IPNet {
+	_, n, err := net.ParseCIDR(s)
+	if err != nil {
+		panic(err)
+	}
+	return n
+}
+
+func runGoBGP(ctx context.Context) (chan *table.Path, error) {
+	s := gobgp.NewBgpServer()
+	go s.Serve()
+
+	global := &config.Global{
+		Config: config.GlobalConfig{
+			As:       64543,
+			RouterId: "1.2.3.4",
+			Port:     4179,
+		},
+	}
+	if err := s.Start(global); err != nil {
+		return nil, err
+	}
+
+	n := &config.Neighbor{
+		Config: config.NeighborConfig{
+			NeighborAddress: "127.0.0.1",
+			PeerAs:          64543,
+		},
+	}
+	if err := s.AddNeighbor(n); err != nil {
+		return nil, err
+	}
+
+	ips := make(chan *table.Path, 1000)
+	w := s.Watch(gobgp.WatchBestPath(false))
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case ev := <-w.Event():
+				switch msg := ev.(type) {
+				case *gobgp.WatchEventBestPath:
+					for _, path := range msg.PathList {
+						ips <- path
+					}
+				}
+			}
+		}
+	}()
+	return ips, nil
+}
+
+func TestInterop(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	ips, err := runGoBGP(ctx)
+	if err != nil {
+		t.Fatalf("starting GoBGP: %s", err)
+	}
+
+	sess, err := New("127.0.0.1:4179", 64543, net.ParseIP("2.3.4.5"), 64543, 10*time.Second)
+	if err != nil {
+		t.Fatalf("starting BGP session to GoBGP: %s", err)
+	}
+	defer sess.Close()
+
+	adv := &Advertisement{
+		Prefix:      ipnet("1.2.3.0/24"),
+		NextHop:     net.ParseIP("10.20.30.40"),
+		LocalPref:   42,
+		Communities: []uint32{1234, 2345},
+	}
+
+	if err := sess.Set(adv); err != nil {
+		t.Fatalf("setting advertisement: %s", err)
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			t.Fatalf("test timed out waiting for route")
+		case path := <-ips:
+			if err := checkPath(path, adv); err != nil {
+				t.Fatalf("path did not match expectations: %s", err)
+			}
+			return
+		}
+	}
+}
+
+func checkPath(path *table.Path, adv *Advertisement) error {
+	nlri := path.GetNlri()
+	if nlri.String() != adv.Prefix.String() {
+		return fmt.Errorf("wrong nlri, got %s, want %s", nlri.String(), adv.Prefix.String())
+	}
+
+	if !path.GetNexthop().Equal(adv.NextHop) {
+		return fmt.Errorf("wrong nexthop, got %s, want %s", path.GetNexthop(), adv.NextHop)
+	}
+
+	lp, err := path.GetLocalPref()
+	if err != nil {
+		return fmt.Errorf("get localpref: %s", err)
+	}
+	if lp != adv.LocalPref {
+		return fmt.Errorf("wrong localpref, got %d, want %d", lp, adv.LocalPref)
+	}
+
+	comms := path.GetCommunities()
+	sort.Slice(comms, func(i, j int) bool { return comms[i] < comms[j] })
+	if diff := cmp.Diff(adv.Communities, comms); diff != "" {
+		return fmt.Errorf("wrong communities (-want +got)\n%s", diff)
+	}
+
+	return nil
+}
