e5c228dab0175526ea061ccc5f0c6bfc6c0f39a7
David Anderson <dave@natulte.net>
2019-07-21 02:07:29 -0700

    Automated update for release v0.8.0

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,7 +3,7 @@ package version
 import "fmt"
 
 var (
-	version   = ""   // Filled out during release cutting
+	version   = "0.8.0"
 	gitCommit string // Provided by ldflags during build
 	gitBranch string // Provided by ldflags during build
 )
