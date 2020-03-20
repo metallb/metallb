d855487bc17b75a267ce9bbcdab336c18e7a0543
David Anderson <dave@natulte.net>
2020-03-20 14:07:17 -0700

    Automated update for release v0.9.2

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,7 +3,7 @@ package version
 import "fmt"
 
 var (
-	version   = "0.9.1"
+	version   = "0.9.2"
 	gitCommit string // Provided by ldflags during build
 	gitBranch string // Provided by ldflags during build
 )
