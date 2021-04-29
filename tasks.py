aaad1d25621eb9c755f18af8b37cc6d7d6992816
Mark Gray <mark.d.gray@redhat.com>
2021-04-29 08:53:59 -0400

    ci: Migrate to golangci-lint
    Migrate linter to "golangci-lint" due to deprecation of
    "gometalinter" as seen by:
    
    https://github.com/alecthomas/gometalinter/issues/590
    
    Also, add linter task to "task.py"
    
    Signed-off-by: Mark Gray <mark.d.gray@redhat.com>

--- tasks.py
+++ tasks.py
@@ -519,3 +519,25 @@ def checkpatch(ctx):
     except UnexpectedExit:
         # Will exit non-zero if no double-space-after-period lines are found.
         pass
+
+
+@task(help={
+    "env": "Specify in which environment to run the linter . Default 'container'. Supported: 'container','host'"
+})
+def lint(ctx, env="container"):
+    """Run linter.
+
+    By default, this will run a golangci-lint docker image against the code.
+    However, in some environments (such as the MetalLB CI), it may be more
+    convenient to install the golangci-lint binaries on the host. This can be
+    achieved by running `inv lint --env host`.
+    """
+    version = "1.39.0"
+
+    if env == "container":
+        run("docker run --rm -v $(git rev-parse --show-toplevel):/app -w /app golangci/golangci-lint:v{} golangci-lint run $(git rev-parse --show-toplevel)/...".format(version), echo=True)
+    elif env == "host":
+        run("curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v{}".format(version))
+        run("golangci-lint run --timeout 5m0s $(git rev-parse --show-toplevel)/...")
+    else:
+        raise Exit(message="Unsupported linter environment: {}". format(env))
