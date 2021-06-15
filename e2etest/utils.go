338ae9124bec456abfaa422d68bdf669382cf3ea
Mohamed Mahmoud <mmahmoud@redhat.com>
2021-06-15 15:54:14 -0400

    Add wget retries to both layer2 and bgp e2e tests
    Signed-off-by: Mohamed Mahmoud <mmahmoud@redhat.com>

--- e2etest/utils.go
+++ e2etest/utils.go
@@ -7,6 +7,11 @@ import (
 	"k8s.io/kubernetes/test/e2e/framework"
 )
 
+const (
+	NetworkFailure = 4
+	retryLimit     = 4
+)
+
 // DescribeSvc logs the output of kubectl describe svc for the given namespace
 func DescribeSvc(ns string) {
 	framework.Logf("\nOutput of kubectl describe svc:\n")
@@ -24,4 +29,27 @@ func runCommand(bgp bool, name string, args ...string) (string, error) {
 	}
 	out, err := exec.Command(name, args...).CombinedOutput()
 	return string(out), err
+}
+
+func wgetRetry(bgp bool, address string) error {
+	retrycnt := 0
+	code := 0
+	var err error
+
+	// Retry loop to handle wget NetworkFailure errors
+	for {
+		_, err = runCommand(bgp, "wget", "-O-", "-q", address, "-T", "60")
+		if exitErr, ok := err.(*exec.ExitError); err != nil && ok {
+			code = exitErr.ExitCode()
+		} else {
+			break
+		}
+		if retrycnt < retryLimit && code == NetworkFailure {
+			framework.Logf(" wget failed with code %d, err %s retrycnt %d\n", code, err, retrycnt)
+			retrycnt++
+		} else {
+			break
+		}
+	}
+	return err
 }
