4ece967a6c4c8236e970a5dd26a8ced6a622d98f
Ori Braunshtein <obraunsh@redhat.com>
2021-10-03 10:55:04 +0300

    FRR: Add Prometheus exporter
    * In order to keep feature-parity with the current
    BGP implementation we need to collect and export
    the same Prometheus metrics.
    
    * With FRR, we can't use the same approach to
    collect the metrics as we currently do by producing the metrics ourselves
    (https://prometheus.io/docs/introduction/glossary/#direct-instrumentation).
    Rather we need to "collect" them from FRR, which is best done
    using a Prometheus exporter and writing a collector for the BGP daemon
    (https://prometheus.io/docs/instrumenting/writing_exporters/).
    Each time Prometheus scrapes the /metrics endpoint the metrics are
    gathered by the collector from FRR using vtysh.
    The collector needs to satisfy the Prometheus.Collector interface.
    
    * Adding new collectors to the exporter per FRR daemon
    is possible in the future.
    
    * The exporter will be built and shipped with the
    speaker, and would be deployed as a seperate sidecar
    container that has access to the FRR sockets like the reloader.
    
    Signed-off-by: Ori Braunshtein <obraunsh@redhat.com>

---
+++ frr-metrics/exporter.go
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier:Apache-2.0
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	stdlog "log"
+	"net/http"
+	"os"
+
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/log/level"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/exporter-toolkit/web"
+
+	"go.universe.tf/metallb/frr-metrics/collector"
+	"go.universe.tf/metallb/internal/logging"
+	"go.universe.tf/metallb/internal/version"
+)
+
+var (
+	metricsPort   = flag.Uint("metrics-port", 7473, "Port to listen on for web interface.")
+	metricsPath   = flag.String("metrics-path", "/metrics", "Path under which to expose metrics.")
+	tlsConfigPath = flag.String("tls-config-path", "", "[EXPERIMENTAL] Path to config yaml file that can enable TLS or authentication.")
+)
+
+func metricsHandler(logger log.Logger) http.Handler {
+	BGPCollector := collector.NewBGP(logger)
+
+	registry := prometheus.NewRegistry()
+	registry.MustRegister(BGPCollector)
+
+	gatherers := prometheus.Gatherers{
+		prometheus.DefaultGatherer,
+		registry,
+	}
+
+	handlerOpts := promhttp.HandlerOpts{
+		ErrorLog:      stdlog.New(log.NewStdlibAdapter(level.Error(logger)), "", 0),
+		ErrorHandling: promhttp.ContinueOnError,
+		Registry:      registry,
+	}
+
+	return promhttp.HandlerFor(gatherers, handlerOpts)
+}
+
+func main() {
+	flag.Parse()
+
+	logger, err := logging.Init("error")
+	if err != nil {
+		fmt.Printf("failed to initialize logging: %s\n", err)
+		os.Exit(1)
+	}
+
+	_ = level.Info(logger).Log("version", version.Version(), "commit", version.CommitHash(), "branch", version.Branch(), "goversion", version.GoString(), "msg", "FRR metrics exporter starting "+version.String())
+
+	http.Handle(*metricsPath, metricsHandler(logger))
+	srv := &http.Server{Addr: fmt.Sprintf(":%d", *metricsPort)}
+	_ = level.Info(logger).Log("msg", "Starting exporter", "metricsPath", metricsPath, "port", metricsPort)
+
+	if err := web.ListenAndServe(srv, *tlsConfigPath, logger); err != nil {
+		_ = level.Error(logger).Log("error", err)
+		os.Exit(1)
+	}
+}
