fdf5744d1a4cb671612b39fd55e2f88276c7234b
Ori Braunshtein <obraunsh@redhat.com>
2021-10-03 10:43:16 +0300

    Refactor and export BGP stats
    Exporting the values used for the Prometheus Gauge/Counter Vecs
    allows us to reuse them when creating other Prometheus types.
    An exporter could use these to create Prometheus.Descs vars.
    
    Signed-off-by: Ori Braunshtein <obraunsh@redhat.com>

--- internal/bgp/native/stats.go
+++ internal/bgp/native/stats.go
@@ -4,42 +4,60 @@ package native
 
 import "github.com/prometheus/client_golang/prometheus"
 
+type stat struct {
+	Name string
+	Help string
+}
+
+var (
+	Namespace = "metallb"
+	Subsystem = "bgp"
+	Labels    = []string{"peer"}
+
+	SessionUp = stat{
+		Name: "session_up",
+		Help: "BGP session state (1 is up, 0 is down)",
+	}
+
+	UpdatesSent = stat{
+		Name: "updates_total",
+		Help: "Number of BGP UPDATE messages sent",
+	}
+
+	Prefixes = stat{
+		Name: "announced_prefixes_total",
+		Help: "Number of prefixes currently being advertised on the BGP session",
+	}
+)
+
 var stats = metrics{
 	sessionUp: prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "metallb",
-		Subsystem: "bgp",
-		Name:      "session_up",
-		Help:      "BGP session state (1 is up, 0 is down)",
-	}, []string{
-		"peer",
-	}),
+		Namespace: Namespace,
+		Subsystem: Subsystem,
+		Name:      SessionUp.Name,
+		Help:      SessionUp.Help,
+	}, Labels),
 
 	updatesSent: prometheus.NewCounterVec(prometheus.CounterOpts{
-		Namespace: "metallb",
-		Subsystem: "bgp",
-		Name:      "updates_total",
-		Help:      "Number of BGP UPDATE messages sent",
-	}, []string{
-		"peer",
-	}),
+		Namespace: Namespace,
+		Subsystem: Subsystem,
+		Name:      UpdatesSent.Name,
+		Help:      UpdatesSent.Help,
+	}, Labels),
 
 	prefixes: prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "metallb",
-		Subsystem: "bgp",
-		Name:      "announced_prefixes_total",
-		Help:      "Number of prefixes currently being advertised on the BGP session",
-	}, []string{
-		"peer",
-	}),
+		Namespace: Namespace,
+		Subsystem: Subsystem,
+		Name:      Prefixes.Name,
+		Help:      Prefixes.Help,
+	}, Labels),
 
 	pendingPrefixes: prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "metallb",
-		Subsystem: "bgp",
+		Namespace: Namespace,
+		Subsystem: Subsystem,
 		Name:      "pending_prefixes_total",
 		Help:      "Number of prefixes that should be advertised on the BGP session",
-	}, []string{
-		"peer",
-	}),
+	}, Labels),
 }
 
 type metrics struct {
