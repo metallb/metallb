7cf905c18207910ff56871d31305b49d1ec175eb
Federico Paolinelli <fpaoline@redhat.com>
2021-12-16 11:12:02 +0100

    E2E: wait for daemons to be available in external frrs (#1120)
    Some tests fails for not having FRR loading the right configuration
    right after starting the container. Here we check for the daemons to be
    ready before giving it away.
    
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

---
+++ e2etest/pkg/frr/daemons.go
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier:Apache-2.0
+
+package frr
+
+import (
+	"strings"
+
+	"github.com/pkg/errors"
+	"go.universe.tf/metallb/e2etest/pkg/executor"
+)
+
+// Daemons returns informations for the all the neighbors in the given
+// executor.
+func Daemons(exec executor.Executor) ([]string, error) {
+	res, err := exec.Exec("vtysh", "-c", "show daemons")
+	if err != nil {
+		return nil, errors.Wrapf(err, "Failed to query neighbours")
+	}
+	res = strings.TrimSuffix(res, "\n")
+	daemons := strings.Split(res, " ")
+	for i := range daemons {
+		daemons[i] = strings.TrimPrefix(daemons[i], " ")
+		daemons[i] = strings.TrimSuffix(daemons[i], " ")
+	}
+	return daemons, nil
+}
