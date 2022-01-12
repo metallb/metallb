9890600218e1ee991e18b15915d2fae98c6424e6
Andrea Panattoni <apanatto@redhat.com>
2022-01-12 13:50:50 +0100

    E2E: Verify configuration in FRR containers (#1143)
    * E2E: Refactor service deletion logic
    
    Slightly improve readability by putting deleteService
    logic to its own function
    
    * E2E: Verify configuration in FRR containers
    
    Add a test with a full BFD and FRR configuration.
    Assertions are based on strings contained in the running configuration,
    as more detailed test are already in place in internal/bgp/frr
    package.
    
    Add config.Peer.KeepaliveTime field to be aligned with the
    production ConfigMap
    
    Add substringCount ginkgo transform function to assert that a string
    appears only once in configuration.
    
    Signed-off-by: Andrea Panattoni <apanatto@redhat.com>

--- e2etest/pkg/config/parse.go
+++ e2etest/pkg/config/parse.go
@@ -27,6 +27,7 @@ type Peer struct {
 	SrcAddr       string         `yaml:"source-address,omitempty"`
 	Port          uint16         `yaml:"peer-port,omitempty"`
 	HoldTime      string         `yaml:"hold-time,omitempty"`
+	KeepaliveTime string         `yaml:"keepalive-time,omitempty"`
 	NodeSelectors []NodeSelector `yaml:"node-selectors,omitempty"`
 	Password      string         `yaml:"password,omitempty"`
 	BFDProfile    string         `yaml:"bfd-profile,omitempty"`
