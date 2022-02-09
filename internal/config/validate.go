96b9f69ac2e3803eeab13d95866d39ea40ece14f
Mohamed S. Mahmoud <mmahmoud@redhat.com>
2022-02-09 15:34:21 -0500

    FRR mode doesn't support duplicate peers in the same VRF (#1190)
    Add check to fail a config with duplicate BGPPeer for FRR
    mode
    
    Signed-off-by: Mohamed Mahmoud <mmahmoud@redhat.com>
    
    Co-authored-by: Federico Paolinelli <fpaoline@redhat.com>

--- internal/config/validate.go
+++ internal/config/validate.go
@@ -56,16 +56,22 @@ func DontValidate(c *configFile) error {
 // DiscardNativeOnly returns an error if the current configFile contains
 // any options that are available only in the native implementation.
 func DiscardNativeOnly(c *configFile) error {
-	if len(c.Peers) > 0 {
+	if len(c.Peers) > 1 {
+		peerAddr := make(map[string]bool)
 		myAsn := c.Peers[0].MyASN
 		routerID := c.Peers[0].RouterID
-		for _, p := range c.Peers {
+		peerAddr[c.Peers[0].Addr] = true
+		for _, p := range c.Peers[1:] {
 			if p.RouterID != routerID {
 				return fmt.Errorf("peer %s has RouterID different from %s, in FRR mode all RouterID must be equal", p.RouterID, c.Peers[0].RouterID)
 			}
 			if p.MyASN != myAsn {
 				return fmt.Errorf("peer %s has myAsn different from %s, in FRR mode all myAsn must be equal", p.Addr, c.Peers[0].Addr)
 			}
+			if _, ok := peerAddr[p.Addr]; ok {
+				return fmt.Errorf("peer %s already exists, FRR mode doesn't support duplicate BGPPeers", p.Addr)
+			}
+			peerAddr[p.Addr] = true
 		}
 	}
 	return nil
