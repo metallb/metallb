255b4dd70e5e4548323b2b6148d4eeb8adf98ca6
Russell Bryant <rbryant@redhat.com>
2022-02-09 16:54:23 -0500

    Automated update for release v0.9.7

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,7 +3,7 @@ package version
 import "fmt"
 
 var (
-	version   = "0.9.6"
+	version   = "0.9.7"
 	gitCommit string // Provided by ldflags during build
 	gitBranch string // Provided by ldflags during build
 )
