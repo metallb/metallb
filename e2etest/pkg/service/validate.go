db3970c7bbd5e5a3984f5dadeb0beb3ecb1a9fce
Federico Paolinelli <fpaoline@redhat.com>
2022-03-24 11:48:48 +0100

    E2E: refactor l2 validation in a function
    We reduce the code duplication and make the function available also to
    other tests.
    
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

---
+++ e2etest/pkg/service/validate.go
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier:Apache-2.0
+
+package service
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+
+	"go.universe.tf/metallb/e2etest/pkg/executor"
+	"go.universe.tf/metallb/e2etest/pkg/wget"
+	corev1 "k8s.io/api/core/v1"
+	e2eservice "k8s.io/kubernetes/test/e2e/framework/service"
+)
+
+func ValidateL2(svc *corev1.Service) error {
+	port := strconv.Itoa(int(svc.Spec.Ports[0].Port))
+	ingressIP := e2eservice.GetIngressPoint(&svc.Status.LoadBalancer.Ingress[0])
+	hostport := net.JoinHostPort(ingressIP, port)
+	address := fmt.Sprintf("http://%s/", hostport)
+
+	err := wget.Do(address, executor.Host)
+	if err != nil {
+		return err
+	}
+	return nil
+}
