e72dcfbc30d1fb5751a02f60817c473e1ec0e1fe
Federico Paolinelli <fpaoline@redhat.com>
2022-04-05 12:35:58 +0200

    Controller: remove unnecessary permissions
    The controller only needs to listen to IPPools / AddressPools, plus the
    crds necessary to implement the webhook.
    We remove the unnecessary permissions and we create a pool controller
    that listens only to ippools / addresspools.
    
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- internal/k8s/listener.go
+++ internal/k8s/listener.go
@@ -16,6 +16,7 @@ type Listener struct {
 	sync.Mutex
 	ServiceChanged func(log.Logger, string, *v1.Service, epslices.EpsOrSlices) controllers.SyncState
 	ConfigChanged  func(log.Logger, *config.Config) controllers.SyncState
+	PoolChanged    func(log.Logger, map[string]*config.Pool) controllers.SyncState
 	NodeChanged    func(log.Logger, *v1.Node) controllers.SyncState
 }
 
@@ -35,4 +36,10 @@ func (l *Listener) NodeHandler(logger log.Logger, node *v1.Node) controllers.Syn
 	l.Lock()
 	defer l.Unlock()
 	return l.NodeChanged(logger, node)
+}
+
+func (l *Listener) PoolHandler(logger log.Logger, pools map[string]*config.Pool) controllers.SyncState {
+	l.Lock()
+	defer l.Unlock()
+	return l.PoolChanged(logger, pools)
 }
