72476a218852e20caf660652267276a35dbe6f20
Federico Paolinelli <fpaoline@redhat.com>
2022-04-12 14:47:07 +0200

    Openshift CI: remove the | escaping and --use-operator
    We are importing upstream changes that remove the escaping, removing
    from CI too.
    
    Also, there's no operator mode anymore as tests are always running using
    crds.
    
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- openshift-ci/run_e2e.sh
+++ openshift-ci/run_e2e.sh
@@ -20,17 +20,17 @@ sudo firewall-cmd --zone=libvirt --add-port=4784/udp
 
 # need to skip L2 metrics test because the pod that's running the tests is not in the
 # same subnet of the cluster nodes, so the arp request that's done in the test won't work.
-SKIP="\"L2 metrics\""
+SKIP="L2 metrics"
 if [ "${IP_STACK}" = "v4" ]; then
-	SKIP="$SKIP\|IPV6\|DUALSTACK"
+	SKIP="$SKIP|IPV6|DUALSTACK"
 	export PROVISIONING_HOST_EXTERNAL_IPV4=${PROVISIONING_HOST_EXTERNAL_IP}
 	export PROVISIONING_HOST_EXTERNAL_IPV6=1111:1:1::1
 elif [ "${IP_STACK}" = "v6" ]; then
-	SKIP="$SKIP\|IPV4\|DUALSTACK"
+	SKIP="$SKIP|IPV4|DUALSTACK"
 	export PROVISIONING_HOST_EXTERNAL_IPV6=${PROVISIONING_HOST_EXTERNAL_IP}
 	export PROVISIONING_HOST_EXTERNAL_IPV4=1.1.1.1
 elif [ "${IP_STACK}" = "v4v6" ]; then
-	SKIP="$SKIP\|IPV6"
+	SKIP="$SKIP|IPV6"
 	export PROVISIONING_HOST_EXTERNAL_IPV4=${PROVISIONING_HOST_EXTERNAL_IP}
 	export PROVISIONING_HOST_EXTERNAL_IPV6=1111:1:1::1
 fi
@@ -43,4 +43,4 @@ export RUN_FRR_CONTAINER_ON_HOST_NETWORK=true
 inv e2etest --kubeconfig=$(readlink -f ../../ocp/ostest/auth/kubeconfig) \
 	--service-pod-port=8080 --system-namespaces="metallb-system" --skip-docker \
 	--ipv4-service-range=192.168.10.0/24 --ipv6-service-range=fc00:f853:0ccd:e799::/124 \
-	--skip="${SKIP}" --use-operator
+	--skip="${SKIP}"
