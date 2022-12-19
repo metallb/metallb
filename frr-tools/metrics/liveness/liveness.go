c14920bda5571102025fb4862a7363c9d7341fe4
Federico Paolinelli <fpaoline@redhat.com>
2022-12-19 17:47:54 +0100

    FRR liveness probe: move the execution inside the container
    Using an exec probe means not binding the resource consumption of the
    execution of the probe to the container, but to the container runtime.
    
    By moving the execution of the vtysh behind the http endpoint, we ensure
    that all the cpu quotas are respected because the actual vtysh execution
    happens in the context of the container.
    
    Despite the liveness probe is related to the frr container, we execute
    it from the frr-metrics container which has already access to vtysh.
    This, in order not to pollute the frr container with an additional
    process, and it is possible since all the containers in the pod share
    the same network namespace.
    
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

---
+++ frr-tools/metrics/liveness/liveness.go
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier:Apache-2.0
+
+package liveness
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/go-kit/log"
+
+	"go.universe.tf/metallb/frr-tools/metrics/vtysh"
+)
+
+func Handler(frrCli vtysh.Cli, logger log.Logger) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		res, err := frrCli("show daemons")
+		if err != nil {
+			http.Error(w, "failed to call show daemons", http.StatusInternalServerError)
+			logger.Log("failed to call show daemons", err)
+			return
+		}
+		expected := map[string]struct{}{
+			"bfdd":     {},
+			"bgpd":     {},
+			"staticd":  {},
+			"watchfrr": {},
+			"zebra":    {},
+		}
+
+		runningDaemons := strings.Split(strings.TrimSuffix(res, "\n"), " ")
+		for _, d := range runningDaemons {
+			delete(expected, d)
+		}
+		if len(expected) > 0 {
+			logger.Log("daemons not running. got: ", res, "missing: ", res)
+			http.NotFound(w, r)
+		}
+	})
+}
