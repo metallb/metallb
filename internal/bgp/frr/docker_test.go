6c7fd6186b968a84276d318b1cb4e2bbd2641ec0
liornoy <lnoy@redhat.com>
2023-01-13 17:29:30 +0200

    Change the FRR image to quay.io/frrouting/frr:7.5.1
    This commit changes the frr image from quay.io/frrouting/frr:stable_7.5 to
    quay.io/frrouting/frr:7.5.1, as image stable_7.5 was removed.
    
    fix #1779

--- internal/bgp/frr/docker_test.go
+++ internal/bgp/frr/docker_test.go
@@ -23,7 +23,7 @@ var (
 )
 
 const (
-	frrImageTag = "v7.5.1"
+	frrImageTag = "7.5.1"
 )
 
 func TestMain(m *testing.M) {
@@ -40,7 +40,7 @@ func TestMain(m *testing.M) {
 	containerHandle, err = pool.RunWithOptions(
 		&dockertest.RunOptions{
 			Name:       "frrtest",
-			Repository: "frrouting/frr",
+			Repository: "quay.io/frrouting/frr",
 			Tag:        frrImageTag,
 			Mounts:     []string{fmt.Sprintf("%s:/etc/tempfrr", frrDir)},
 		},
