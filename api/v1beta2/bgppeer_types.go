31c04037bb8f4471f4332ad11a738327179e9ace
Alina Sudakov <asudakov@redhat.com>
2023-10-26 11:51:43 +0300

    API: Added the connectTime Field to the BGPPeer CRD
    Added the connectTime Field to the BGPPeer CRD. Included a validation rule that validates that the connect field time is set to a whole number of seconds and is between 1 seconds to 65535 as described in frr docs.
    Also Regenerated deepcopy functions, manifests and website api docs.
    
    
    Signed-off-by: Alina Sudakov <asudakov@redhat.com>

--- api/v1beta2/bgppeer_types.go
+++ api/v1beta2/bgppeer_types.go
@@ -55,6 +55,12 @@ type BGPPeerSpec struct {
 	// +optional
 	KeepaliveTime metav1.Duration `json:"keepaliveTime,omitempty"`
 
+	// Requested BGP connect time, controls how long BGP waits between connection attempts to a neighbor.
+	// +kubebuilder:validation:XValidation:message="connect time should be between 1 seconds to 65535",rule="duration(self).getSeconds() >= 1 && duration(self).getSeconds() <= 65535"
+	// +kubebuilder:validation:XValidation:message="connect time should contain a whole number of seconds",rule="duration(self).getMilliseconds() % 1000 == 0"
+	// +optional
+	ConnectTime *metav1.Duration `json:"connectTime,omitempty"`
+
 	// BGP router ID to advertise to the peer
 	// +optional
 	RouterID string `json:"routerID,omitempty"`
