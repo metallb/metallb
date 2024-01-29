e15361ab3b1de94077b3091f7fc410fb1c85005c
lwabish <wubw@pku.edu.cn>
2024-01-29 09:27:48 +0800

    feat: layer2 status api
    Add a new go struct called ServiceL2Status for later implements
    of exposing layer2 service status.
    
    Signed-off-by: lwabish <wubw@pku.edu.cn>
    (cherry picked from commit a4f26542ed0ad3d96864b7debb750d2a11f630b3)

---
+++ api/v1beta1/servicel2status_types.go
@@ -0,0 +1,68 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +kubebuilder:object:generate=true
+
+// MetalLBServiceL2Status defines the observed state of ServiceL2Status.
+type MetalLBServiceL2Status struct {
+	// Node indicates the node that receives the directed traffic
+	Node string `json:"node,omitempty"`
+	// Interfaces indicates the interfaces that receive the directed traffic
+	Interfaces []InterfaceInfo `json:"interfaces,omitempty"`
+}
+
+// InterfaceInfo defines interface info of layer2 announcement.
+type InterfaceInfo struct {
+	// Name the name of network interface card
+	Name string `json:"name,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:storageversion
+// +kubebuilder:printcolumn:name="Allocated Node",type=string,JSONPath=`.status.node`
+
+// ServiceL2Status reveals the actual traffic status of loadbalancer services in layer2 mode.
+type ServiceL2Status struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ServiceL2StatusSpec    `json:"spec,omitempty"`
+	Status MetalLBServiceL2Status `json:"status,omitempty"`
+}
+
+// ServiceL2StatusSpec defines the desired state of ServiceL2Status.
+type ServiceL2StatusSpec struct {
+}
+
+// +kubebuilder:object:root=true
+
+// ServiceL2StatusList contains a list of ServiceL2Status.
+type ServiceL2StatusList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ServiceL2Status `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ServiceL2Status{}, &ServiceL2StatusList{})
+}
