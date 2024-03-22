b3310442717f0ed9859958d578ff2b39481a3394
Federico Paolinelli <fpaoline@redhat.com>
2024-03-22 18:19:29 +0100

    Pin setup-envtest to a specific version
    Currently being bitten by https://github.com/kubernetes-sigs/controller-runtime/issues/2720
    
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- dev-env/unittest/setup-envtest.sh
+++ dev-env/unittest/setup-envtest.sh
@@ -3,4 +3,4 @@
 bin_dir=$1/bin/
 
 mkdir -p ${bin_dir}
-GOBIN=${bin_dir} go install sigs.k8s.io/controller-runtime/tools/setup-envtest@latest
+GOBIN=${bin_dir} go install sigs.k8s.io/controller-runtime/tools/setup-envtest@c7e1dc9
