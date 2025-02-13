2333cdd7db304b9c73ca2af5ac24f4210f36bf0b
Federico Paolinelli <fpaoline@redhat.com>
2025-02-13 18:57:18 +0100

    Bump the golangci-lint version
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- tasks.py
+++ tasks.py
@@ -1128,7 +1128,7 @@ def lint(ctx, env="container"):
     convenient to install the golangci-lint binaries on the host. This can be
     achieved by running `inv lint --env host`.
     """
-    version = "1.59.1"
+    version = "1.63.4"
     golangci_cmd = "golangci-lint run --timeout 10m0s ./..."
 
     if env == "container":
