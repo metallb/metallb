50315a3c93d8d34af3c739b71df163c6843bc92c
Konstantinos Karampogias <karampok@gmail.com>
2025-06-16 10:32:26 +0200

    E2E: refactor debug container executor and add go client implementation
    I observed that the existing implementation works correctly for commands
    that return instantly like whoami, ifconfig but no for commands that
    take time to return and that they fail
    
    e.g.
    ```
    kubectl debug -it -n metallb-system -c controller-debugger-xxx
    --target=controller --image=registry.k8s.io/e2e-test-images/agnhost:2.47
    controller-765bbcb6fc-dzcv4 -- ./agnhost connect 10.244.1.7:8080 --timeout 5s
    
    Targeting container "controller". If you don't see processes from this
    container it may be because the container runtime doesn't support this
    feature.
    If you don't see a command prompt, try pressing enter.
    
     ---  hanging for a bit ---
    
    TIMEOUT
    Session ended, the ephemeral container will not be restarted but may be
    reattached using 'kubectl attach controller-765bbcb6fc-dzcv4 -c
    controller-debugger-xxx -i -t' if it is still running
    ```
    
    For the commands that take time to return, current implementation is not
    waiting and returns before the actual command finishes (probably due to
    how TTY works), and therefore it returns neither the correct output (=
    only the output from the command we want to run), neither the error (if
    the command fails the kubectl debug actually returns ok).
    
    So this commit refactors the code and follows the logic where the debug
    container is a container attached to the pod, and we can do from there
    normal `kubectl exec` commands without the `-it` argument and that
    enables the e2etest for networkpolicies.
    
    https://kubernetes.io/docs/tasks/debug/debug-application/debug-running-pod/#ephemeral-container
    
    Signed-off-by: Konstantinos Karampogias <karampok@gmail.com>

--- e2etest/pkg/executor/executor.go
+++ e2etest/pkg/executor/executor.go
@@ -4,12 +4,17 @@ package executor
 
 import (
 	"bytes"
+	"context"
 	"errors"
 	"fmt"
 	"os"
 	"os/exec"
+	"time"
 
-	utilrand "k8s.io/apimachinery/pkg/util/rand"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	clientset "k8s.io/client-go/kubernetes"
 )
 
 var Kubectl string
@@ -69,13 +74,12 @@ func (p *podExecutor) Exec(cmd string, args ...string) (string, error) {
 		return "", errors.New("the kubectl parameter is not set")
 	}
 	fullargs := append([]string{"exec", p.name, "-n", p.namespace, "-c", p.container, "--", cmd}, args...)
-
 	c := exec.Command(Kubectl, fullargs...)
 	var stdoutBuf, stderrBuf bytes.Buffer
 	c.Stdout = &stdoutBuf
 	c.Stderr = &stderrBuf
 	if err := c.Run(); err != nil {
-		return "", fmt.Errorf("kubectl exec failed: %w, stderr: %s", err, stderrBuf.String())
+		return stderrBuf.String(), fmt.Errorf("kubectl exec failed: %w, stderr: %s", err, stderrBuf.String())
 	}
 	return stdoutBuf.String(), nil
 }
@@ -86,27 +90,80 @@ type podDebugExecutor struct {
 	name      string
 	container string
 	image     string
+	ephemeral string
+	cs        clientset.Interface
 }
 
-func ForPodDebug(namespace, name, container, image string) Executor {
-	return &podDebugExecutor{
+func ForPodDebug(cs clientset.Interface, namespace, podName, targetContainer, image string) (Executor, error) {
+	ret := &podDebugExecutor{
 		namespace: namespace,
-		name:      name,
-		container: container,
-		image:     image,
+		name:      podName,
+		container: targetContainer,
+		ephemeral: "debugger-" + targetContainer,
+		cs:        cs,
 	}
+
+	ephemeralContainer := corev1.EphemeralContainer{
+		EphemeralContainerCommon: corev1.EphemeralContainerCommon{
+			Name:  ret.ephemeral,
+			Image: image,
+		},
+		TargetContainerName: targetContainer,
+	}
+
+	ctx := context.Background()
+	pod, err := cs.CoreV1().Pods(namespace).Get(ctx, podName, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+	containerExists := false
+	for _, ec := range pod.Spec.EphemeralContainers {
+		if ec.Name == ret.ephemeral {
+			containerExists = true
+			break
+		}
+	}
+
+	if !containerExists {
+		if err := addEphemeralContainerToPod(ctx, cs, pod, ephemeralContainer); err != nil {
+			return nil, fmt.Errorf("failed to create ephemeral container: %w", err)
+		}
+	}
+
+	err = wait.PollUntilContextTimeout(context.Background(), 1*time.Second, 20*time.Second, false, func(context.Context) (bool, error) {
+		updatedPod, err := cs.CoreV1().Pods(namespace).Get(context.TODO(), podName, metav1.GetOptions{})
+		if err != nil {
+			return false, nil
+		}
+
+		for _, status := range updatedPod.Status.EphemeralContainerStatuses {
+			if status.Name == ret.ephemeral {
+				return status.State.Running != nil, nil
+			}
+		}
+		return false, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return ret, nil
 }
 
 func (pd *podDebugExecutor) Exec(cmd string, args ...string) (string, error) {
 	if Kubectl == "" {
 		return "", errors.New("the kubectl parameter is not set")
 	}
-
-	imageArg := "--image=" + pd.image
-	targetArg := "--target=" + pd.container
-	debuggerArg := pd.container + "-debugger-" + utilrand.String(5)
-
-	fullargs := append([]string{"debug", "-it", "-n", pd.namespace, "-c", debuggerArg, targetArg, imageArg, pd.name, "--", cmd}, args...)
+	fullargs := append([]string{"exec", "-n", pd.namespace, "-c", pd.ephemeral, pd.name, "--", cmd}, args...)
 	out, err := exec.Command(Kubectl, fullargs...).CombinedOutput()
 	return string(out), err
+}
+
+func addEphemeralContainerToPod(ctx context.Context, cs clientset.Interface, pod *corev1.Pod, ephemeralContainer corev1.EphemeralContainer) error {
+	pod.Spec.EphemeralContainers = append(pod.Spec.EphemeralContainers, ephemeralContainer)
+	_, err := cs.CoreV1().Pods(pod.Namespace).UpdateEphemeralContainers(ctx, pod.Name, pod, metav1.UpdateOptions{})
+	if err != nil {
+		return err
+	}
+	return nil
 }
