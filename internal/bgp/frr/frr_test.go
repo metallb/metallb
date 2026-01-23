54fb789ccffb2ad7d91e8a3ba526e9e8ec44c69c
Andreas Karis <ak.karis@gmail.com>
2026-01-24 00:14:18 +0100

    Unit tests: Fix wrong environment variable configuration
    TestLoggingConfigurationOverrideByEnvironmentVar read env var via
    os.Getenv (which, if unset, yields "") and would then set the env
    variable for all subsequent tests explicitly to "". Use t.Setenv
    instead.
    
    Reported-at: https://github.com/metallb/metallb/issues/2929
    Signed-off-by: Andreas Karis <ak.karis@gmail.com>

--- internal/bgp/frr/frr_test.go
+++ internal/bgp/frr/frr_test.go
@@ -1016,9 +1016,7 @@ func TestLoggingConfigurationDebug(t *testing.T) {
 func TestLoggingConfigurationOverrideByEnvironmentVar(t *testing.T) {
 	testSetup(t)
 
-	orig := os.Getenv("FRR_LOGGING_LEVEL")
-	os.Setenv("FRR_LOGGING_LEVEL", "alerts")
-	t.Cleanup(func() { os.Setenv("FRR_LOGGING_LEVEL", orig) })
+	t.Setenv("FRR_LOGGING_LEVEL", "alerts")
 
 	l := log.NewNopLogger()
 	sessionManager := mockNewSessionManager(l, logging.LevelDebug)
