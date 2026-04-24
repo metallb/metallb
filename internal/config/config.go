96e6556c0b938c749fd216d1539d833d4ef626ef
Andreas Karis <ak.karis@gmail.com>
2026-04-24 17:28:37 +0200

    Rewrite config.go selectedNodes and selectedPools
    The function names were ambiguous. Rename them to
    FilterNodesWithLabelSelector and filterPoolsWithLabelSelector,
    respectively.
    
    Export the first function as it is needed by a follow-up commit.
    As FilterNodesWithLabelSelector is needed by a follow-up commit, modify
    its behaviour slightly: the function now always filters according to
    the selectors. Instead, check for length of the selector in the
    caller, and if len(nodeSelectors) == 0 use new helper
    extractNodeNames(nodes) to get the full list of node names.
    
    Have both functions return a new type Set map[string]bool.
    
    The new resulting functions share a lot of commonality: extract the
    business logic into a helper.
    
    Signed-off-by: Andreas Karis <ak.karis@gmail.com>
    
    fixup
    
    Signed-off-by: Andreas Karis <ak.karis@gmail.com>

--- internal/config/config.go
+++ internal/config/config.go
@@ -672,7 +672,7 @@ func setL2AdvertisementsToPools(ipPools []metallbv1beta1.IPAddressPool, l2Advs [
 		if err != nil {
 			return err
 		}
-		ipPoolsSelected, err := selectedPools(ipPools, l2Adv.Spec.IPAddressPoolSelectors)
+		ipPoolsSelected, err := filterPoolsWithLabelSelector(ipPools, l2Adv.Spec.IPAddressPoolSelectors)
 		if err != nil {
 			return err
 		}
@@ -685,7 +685,7 @@ func setL2AdvertisementsToPools(ipPools []metallbv1beta1.IPAddressPool, l2Advs [
 			}
 			continue
 		}
-		for _, poolName := range append(l2Adv.Spec.IPAddressPools, ipPoolsSelected...) {
+		for _, poolName := range append(l2Adv.Spec.IPAddressPools, ipPoolsSelected.UnsortedList()...) {
 			if pool, ok := ipPoolMap[poolName]; ok {
 				if !containsAdvertisement(pool.L2Advertisements, adv) {
 					pool.L2Advertisements = append(pool.L2Advertisements, adv)
@@ -707,7 +707,7 @@ func setBGPAdvertisementsToPools(ipPools []metallbv1beta1.IPAddressPool, bgpAdvs
 		if err != nil {
 			return err
 		}
-		ipPoolsSelected, err := selectedPools(ipPools, bgpAdv.Spec.IPAddressPoolSelectors)
+		ipPoolsSelected, err := filterPoolsWithLabelSelector(ipPools, bgpAdv.Spec.IPAddressPoolSelectors)
 		if err != nil {
 			return err
 		}
@@ -722,7 +722,7 @@ func setBGPAdvertisementsToPools(ipPools []metallbv1beta1.IPAddressPool, bgpAdvs
 			}
 			continue
 		}
-		for _, poolName := range append(bgpAdv.Spec.IPAddressPools, ipPoolsSelected...) {
+		for _, poolName := range append(bgpAdv.Spec.IPAddressPools, ipPoolsSelected.UnsortedList()...) {
 			if pool, ok := ipPoolMap[poolName]; ok {
 				err := validateBGPAdvPerPool(adv, pool)
 				if err != nil {
@@ -752,9 +752,14 @@ func l2AdvertisementFromCR(crdAd metallbv1beta1.L2Advertisement, nodes []corev1.
 	if err != nil {
 		return nil, err
 	}
-	selected, err := selectedNodes(nodes, crdAd.Spec.NodeSelectors)
-	if err != nil {
-		return nil, errors.Join(err, fmt.Errorf("failed to parse node selector for %s", crdAd.Name))
+	var selected map[string]bool
+	if len(crdAd.Spec.NodeSelectors) == 0 {
+		selected = extractNodeNames(nodes)
+	} else {
+		selected, err = FilterNodesWithLabelSelector(nodes, crdAd.Spec.NodeSelectors)
+		if err != nil {
+			return nil, errors.Join(err, fmt.Errorf("failed to parse node selector for %s", crdAd.Name))
+		}
 	}
 	serviceSelectors, err := parseSelectors(crdAd.Spec.ServiceSelectors)
 	if err != nil {
@@ -842,9 +847,14 @@ func bgpAdvertisementFromCR(crdAd metallbv1beta1.BGPAdvertisement, communities m
 		ad.Communities[v] = true
 	}
 
-	selected, err := selectedNodes(nodes, crdAd.Spec.NodeSelectors)
-	if err != nil {
-		return nil, errors.Join(err, fmt.Errorf("failed to parse node selector for %s", crdAd.Name))
+	var selected map[string]bool
+	if len(crdAd.Spec.NodeSelectors) == 0 {
+		selected = extractNodeNames(nodes)
+	} else {
+		selected, err = FilterNodesWithLabelSelector(nodes, crdAd.Spec.NodeSelectors)
+		if err != nil {
+			return nil, errors.Join(err, fmt.Errorf("failed to parse node selector for %s", crdAd.Name))
+		}
 	}
 	ad.Nodes = selected
 
@@ -1138,56 +1148,6 @@ func parseSelectors(selectors []metav1.LabelSelector) ([]labels.Selector, error)
 	return result, nil
 }
 
-func selectedNodes(nodes []corev1.Node, selectors []metav1.LabelSelector) (map[string]bool, error) {
-	labelSelectors := []labels.Selector{}
-	for _, selector := range selectors {
-		l, err := metav1.LabelSelectorAsSelector(&selector)
-		if err != nil {
-			return nil, errors.Join(err, fmt.Errorf("invalid label selector %v", selector))
-		}
-		labelSelectors = append(labelSelectors, l)
-	}
-
-	res := make(map[string]bool)
-OUTER:
-	for _, node := range nodes {
-		if len(labelSelectors) == 0 { // no selector mean all nodes are valid
-			res[node.Name] = true
-		}
-		for _, s := range labelSelectors {
-			nodeLabels := labels.Set(node.Labels)
-			if s.Matches(nodeLabels) {
-				res[node.Name] = true
-				continue OUTER
-			}
-		}
-	}
-	return res, nil
-}
-
-func selectedPools(pools []metallbv1beta1.IPAddressPool, selectors []metav1.LabelSelector) ([]string, error) {
-	labelSelectors := []labels.Selector{}
-	for _, selector := range selectors {
-		l, err := metav1.LabelSelectorAsSelector(&selector)
-		if err != nil {
-			return nil, errors.Join(err, fmt.Errorf("invalid label selector %v", selector))
-		}
-		labelSelectors = append(labelSelectors, l)
-	}
-	var ipPools []string
-OUTER:
-	for _, pool := range pools {
-		for _, s := range labelSelectors {
-			poolLabels := labels.Set(pool.Labels)
-			if s.Matches(poolLabels) {
-				ipPools = append(ipPools, pool.Name)
-				continue OUTER
-			}
-		}
-	}
-	return ipPools, nil
-}
-
 func validateLabelSelectorDuplicate(labelSelectors []metav1.LabelSelector, labelSelectorType string) error {
 	for _, ls := range labelSelectors {
 		for _, me := range ls.MatchExpressions {
@@ -1232,4 +1192,72 @@ func validateDuplicate(strSlice []string, sliceType string) error {
 		}
 	}
 	return nil
+}
+
+// Set represents a set. It's similar to k8s Set but uses map[string]bool as it's backing store for compatibility with
+// L2Advertisement's and BGPAdvertisement's `Nodes map[string]bool` field.
+type Set map[string]bool
+
+// UnsortedList returns an unsorted list from the set.
+func (s Set) UnsortedList() []string {
+	list := make([]string, 0, len(s))
+	for item := range s {
+		list = append(list, item)
+	}
+	return list
+}
+
+// FilterNodesWithLabelSelector returns a Set of nodes that match any of the provided label selectors.
+func FilterNodesWithLabelSelector(nodes []corev1.Node, selectors []metav1.LabelSelector) (Set, error) {
+	return filterWithLabelSelector(
+		nodes,
+		selectors,
+		func(p corev1.Node) map[string]string { return p.Labels },
+		func(p corev1.Node) string { return p.Name },
+	)
+}
+
+// filterPoolsWithLabelSelector returns a Set of IPAddressPools that match any of the provided label selectors.
+func filterPoolsWithLabelSelector(ipPools []metallbv1beta1.IPAddressPool, selectors []metav1.LabelSelector) (Set, error) {
+	return filterWithLabelSelector(
+		ipPools,
+		selectors,
+		func(p metallbv1beta1.IPAddressPool) map[string]string { return p.Labels },
+		func(p metallbv1beta1.IPAddressPool) string { return p.Name },
+	)
+}
+
+// filterWithLabelSelector implements the business logic for the more specific filter functions such as
+// FilterNodesWithLabelSelector.
+func filterWithLabelSelector[T any](toFilterSlice []T, selectors []metav1.LabelSelector,
+	getLabels func(T) map[string]string, getName func(T) string) (Set, error) {
+	labelSelectors := []labels.Selector{}
+	for _, selector := range selectors {
+		l, err := metav1.LabelSelectorAsSelector(&selector)
+		if err != nil {
+			return nil, errors.Join(err, fmt.Errorf("invalid label selector %v", selector))
+		}
+		labelSelectors = append(labelSelectors, l)
+	}
+	filteredSet := Set{}
+OUTER:
+	for _, obj := range toFilterSlice {
+		objLabels := labels.Set(getLabels(obj))
+		for _, s := range labelSelectors {
+			if s.Matches(objLabels) {
+				filteredSet[getName(obj)] = true
+				continue OUTER
+			}
+		}
+	}
+	return filteredSet, nil
+}
+
+// extractNodeNames iterates over the provided slice of Nodes and returns a set with their names.
+func extractNodeNames(nodes []corev1.Node) Set {
+	nodeNames := Set{}
+	for _, node := range nodes {
+		nodeNames[node.GetName()] = true
+	}
+	return nodeNames
 }
